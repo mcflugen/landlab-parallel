cf7b8fbaa3ab0d1072d6919e776e95b99a0d1832
mcflugen <mcflugen@gmail.com>
2025-07-31 23:22:44 -0600

    use pyargs option in test session

--- noxfile.py
+++ noxfile.py
@@ -40,19 +40,10 @@ def install(session: nox.Session) -> None:
 @nox.session
 def test(session: nox.Session) -> None:
     """Run the tests."""
-    session.install("coverage", "pytest")
+    session.install("pytest")
     install(session)
 
-    session.run(
-        "coverage",
-        "run",
-        "--branch",
-        "--source=landlab_parallel,tests",
-        "--module",
-        "pytest",
-    )
-    session.run("coverage", "report", "--ignore-errors", "--show-missing")
-    session.run("coverage", "xml", "-o", "coverage.xml")
+    session.run("pytest", "--doctest-modules", "--pyargs", "landlab_parallel")
 
 
 @nox.session
@@ -67,7 +58,6 @@ def coverage(session: nox.Session) -> None:
         "-m",
         "pytest",
         "src/landlab_parallel",
-        "tests",
         "--doctest-modules",
         env={"COVERAGE_CORE": "sysmon"},
     )
